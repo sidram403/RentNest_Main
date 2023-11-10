ddbc9753b5309672af01097431b575a358f1750d
Siddharth Patil <sid@Siddharths-MacBook-Pro.local>
2023-11-10 18:30:21 +0530

    added react redux toolkit

--- client/src/pages/SingIn.jsx
+++ client/src/pages/SingIn.jsx
@@ -1,12 +1,17 @@
 import React, { useState } from "react";
-import { Link , useNavigate} from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  signInStart,
+  signInSucess,
+  signInFailure,
+} from "../redux/user/userSlice";
 
 const SignIn = () => {
   const [formData, setFormData] = useState({});
-  const [error, setError] = useState(null)
-  const [loading, setLoading] = useState(false)
+  const {loading, error} = useSelector((state) => state.user)
   const navigate = useNavigate();
-  
+  const dispatch = useDispatch();
   const handleChange = (e) => {
     setFormData({
       ...formData,
@@ -14,34 +19,29 @@ const SignIn = () => {
     });
   };
 
-  const handleSubmit = async (e) =>{
+  const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      setLoading(true)
-    const res = await fetch('/api/auth/signin',
-    {
-      method: 'POST',
-      headers:{
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(formData),
-    }
-    )
-    const data = await res.json();
-    if(data.success === false){
-      setError(data.message);
-      setLoading(false);
-      return;
-    }
-    setLoading(false);
-    setError(null)
-    navigate('/')
+      dispatch(signInStart());
+      const res = await fetch("/api/auth/signin", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        dispatch(signInFailure(data.message))
+        return;
+      }
+      dispatch(signInSucess(data))
+      navigate("/");
     } catch (error) {
-      setLoading(false);
-      setError(error.message)
+      dispatch(signInFailure(error.message))
+
     }
-    
-  }
+  };
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl text-center font-semibold my-7"> Sign In</h1>
@@ -54,7 +54,7 @@ const SignIn = () => {
           id="email"
           onChange={handleChange}
         />
-       
+
         <input
           type="password"
           placeholder="Password"
@@ -62,19 +62,20 @@ const SignIn = () => {
           id="password"
           onChange={handleChange}
         />
-        <button disabled={loading} className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80">
+        <button
+          disabled={loading}
+          className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80"
+        >
           {loading ? "Loading..." : "Sign In"}
         </button>
       </form>
       <div className="flex gap-2 mt-5">
         <p>Dont have an account?</p>
-        <Link to="/sign-in">
+        <Link to="/sign-up">
           <span className="text-blue-700">Sign Up</span>
         </Link>
       </div>
-      {error && 
-       <p className="text-red-500 mt-5">{error}</p>
-      }
+      {error && <p className="text-red-500 mt-5">{error}</p>}
     </div>
   );
 };
