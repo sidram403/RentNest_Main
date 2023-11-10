a06051c0e17ec0dffcd62f4ea7b8c62b0556f073
Siddharth Patil <sid@Siddharths-MacBook-Pro.local>
2023-11-10 14:03:05 +0530

    created signup api route

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -2,19 +2,19 @@ import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
     username:{
-        type:string,
+        type:String,
         required:true,
         unique: true,
 
     },
     email:{
-        type:string,
+        type:String,
         required:true,
         unique: true,
 
     },
     password:{
-        type:string,
+        type:String,
         required:true,
 
     },
