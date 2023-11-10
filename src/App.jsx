d3d83604324655a39f4fc3bc699f9cbe83962d3d
Siddharth Patil <sid@Siddharths-MacBook-Pro.local>
2023-11-10 12:13:37 +0530

    created header section

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,12 @@ import SingIn from './pages/SingIn'
 import Signup from './pages/Signup'
 import About from './pages/About'
 import Profile from './pages/Profile'
+import Header from './components/Header'
 
 const App = () => {
   return (
     <BrowserRouter>
+    <Header />
      <Routes>
         <Route path='/' element={<Home />}></Route>
         <Route path='/sign-in' element={<SingIn />}></Route>
---
+++ src/components/Header.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { FaSearch } from "react-icons/fa";
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <header className="bg-slate-200 shadow-md">
+      <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
+        <Link to="/">
+          <div className="logo__section w-28 sm:w-48">
+            <img src="../../public/logo.png" alt="img" />
+          </div>
+        </Link>
+        <form className="bg-slate-100 p-3 rounded-lg flex items-center">
+          <input
+            type="text"
+            placeholder="Search..."
+            className="bg-transparent focus:outline-none w-24 sm:w-64"
+          />
+          <FaSearch className="text-slate-600" />
+        </form>
+        <ul className="flex gap-4">
+          <Link to="/">
+            <li className="hidden sm:inline text-slate-700 hover:underline cursor-pointer">
+              Home
+            </li>
+          </Link>
+          <Link to="/about">
+            <li className="hidden sm:inline text-slate-700 hover:underline cursor-pointer">
+              About
+            </li>
+          </Link>
+          <Link to="/sign-in">
+            <li className="text-slate-700 hover:underline cursor-pointer">
+              {" "}
+              Sing in
+            </li>
+          </Link>
+        </ul>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
