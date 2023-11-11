9601a5ca71170ab39ac7d7cca13ef1d76d89b54a
Siddharth Patil <sid@Siddharths-MacBook-Pro.local>
2023-11-12 01:37:39 +0530

    created header and make profile page private

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,7 @@ import Signup from './pages/Signup'
 import About from './pages/About'
 import Profile from './pages/Profile'
 import Header from './components/Header'
+import PrivateRoute from './components/PrivateRoute'
 
 const App = () => {
   return (
@@ -16,7 +17,9 @@ const App = () => {
         <Route path='/sign-in' element={<SingIn />}></Route>
         <Route path='/sign-up' element={<Signup />}></Route>
         <Route path='/about' element={<About />}></Route>
-        <Route path='/profile' element={<Profile />}></Route>
+        <Route element={<PrivateRoute />}>
+          <Route path='/profile' element={<Profile />}></Route>
+        </Route>
      </Routes>
     </BrowserRouter>
   )
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import { FaSearch } from "react-icons/fa";
 import { Link } from "react-router-dom";
+import {  useSelector } from "react-redux";
 
 const Header = () => {
+  const {currentUser} = useSelector(state => state.user);
   return (
     <header className="bg-slate-200 shadow-md">
       <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
@@ -30,11 +32,19 @@ const Header = () => {
               About
             </li>
           </Link>
-          <Link to="/sign-in">
+          
+          <Link to="/profile">
+          {currentUser ? (
+              <img className="rounded-full h-7 w-7 object-cover" src={currentUser.avatar} alt="profile pic" />
+          ) : (
             <li className="text-slate-700 hover:underline cursor-pointer">
-              {" "}
-              Sing in
-            </li>
+            {" "}
+            Sing in
+          </li>
+          )
+          
+        }
+           
           </Link>
         </ul>
       </div>
