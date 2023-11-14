c5887e0eefd12c2836a37c63b609b452759e8201
Siddharth Patil <sid@Siddharths-MacBook-Pro.local>
2023-11-14 18:20:25 +0530

    added sign out functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useRef, useState } from 'react'
 import {getStorage, uploadBytesResumable, ref, getDownloadURL} from 'firebase/storage'
 import { useSelector } from 'react-redux'
 import { app } from '../firebase';
-import { updateUserStart, updateUserSuccess, updateUserFailure, deleteUserFailure, deleteUserStart, deleteUserSuccess } from '../redux/user/userSlice';
+import { updateUserStart, updateUserSuccess, updateUserFailure, deleteUserFailure, deleteUserStart, deleteUserSuccess, signOutUserStart, signOutUserSuccess, signOutUserFailure } from '../redux/user/userSlice';
 import { useDispatch } from 'react-redux';
 
 
@@ -98,6 +98,21 @@ const Profile = () => {
     }
   }
 
+  const handleSignOut = async ()=>{
+      try {
+        dispatch(signOutUserStart())
+        const res = await fetch('/api/auth/signout');
+        const data = await res.json();
+        if(data.success === false){
+          dispatch(signOutUserFailure(data.message));
+          return;
+        }
+        dispatch(signOutUserSuccess(data))
+      } catch (error) {
+        dispatch(signOutUserFailure(data.message));
+      }
+  }
+
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
@@ -127,7 +142,7 @@ const Profile = () => {
       </form>
       <div className='flex justify-between mt-5'>
         <span onClick={handleDeleteUser} className='text-red-700 cursor-pointer'>Delete Account</span>
-        <span className='text-red-700 cursor-pointer'>Sign Out</span>
+        <span onClick={handleSignOut} className='text-red-700 cursor-pointer'>Sign Out</span>
       </div>
       <p className='text-red-700 mt-5'>{error ? error :''}</p>
       <p className='text-green-700 mt-5'>{updateSuccess ? "User is updated successfully":""}</p>
